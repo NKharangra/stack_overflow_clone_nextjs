d5a8a0905ab4b55550bafb2eb7b61d47c06ef715
NKharangra <107696311+NKharangra@users.noreply.github.com>
2024-01-10 03:30:30 +1100

    config file updated to remove experimental feature

--- next.config.js
+++ next.config.js
@@ -1,22 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-    mdxRs: true,
-    serverComponentsExternalPackages: ['mongoose']
-  },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '*'
-      }, 
+        protocol: "https",
+        hostname: "*",
+      },
       {
-        protocol: 'http',
-        hostname: '*'
-      }, 
-    ]
-  }
-}
+        protocol: "http",
+        hostname: "*",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
